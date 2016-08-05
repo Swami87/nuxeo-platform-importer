5a2602de350d06c0643203665fcf0e4b4c5fc885
Andrei Nechaev <anechaev@nuxeo.com>
2016-08-05 09:46:58 -0400

    NXP-20215 headers

--- nuxeo-importer-broker/src/main/java/org/nuxeo/ecm/platform/importer/kafka/settings/ServiceHelper.java
+++ nuxeo-importer-broker/src/main/java/org/nuxeo/ecm/platform/importer/kafka/settings/ServiceHelper.java
@@ -7,8 +7,20 @@ import java.io.InputStream;
 import java.util.Properties;
 
 /**
- * Created by anechaev on 7/25/16.
- * © Andrei Nechaev 2016
+ * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Andrei Nechaev
  */
 
 public class ServiceHelper {
--- nuxeo-importer-broker/src/main/java/org/nuxeo/ecm/platform/importer/kafka/settings/Settings.java
+++ nuxeo-importer-broker/src/main/java/org/nuxeo/ecm/platform/importer/kafka/settings/Settings.java
@@ -1,9 +1,22 @@
 package org.nuxeo.ecm.platform.importer.kafka.settings;
 
 /**
- * Created by anechaev on 7/25/16.
- * © Andre Nechaev 2016
+ * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Andrei Nechaev
  */
+
 public class Settings {
     public static final String ZOOKEEPER = "zk_props";
     public static final String KAFKA = "kafka_props";
